a02a5f536be1a5554e864d2c56e3ffa3f0addb50
PriyankaKale14 <150790740+PriyankaKale14@users.noreply.github.com>
2023-11-16 14:56:52 +0530

    Update Operations.java

--- Operations.java
+++ Operations.java
@@ -10,7 +10,7 @@ import java.util.*;
 public class Operations {
     public static void main(String[] args) 
     {
-        System.out.println("1]Find out greater between 3 NO\n2]Check given no is even or odd\n3]Checkk the given character is vowel or not\n4]check the given no is divible by 5 or not\n");
+        System.out.println("1-Find out greater between 3 NO\n2-Check given no is even or odd\n3-Checkk the given character is vowel or not\n4-check the given no is divible by 5 or not\n");
 	//To accept the values from user
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter your choice=");
@@ -19,14 +19,14 @@ public class Operations {
     switch (choice) {
         case 1:
             System.out.println("To find greatest between 3 numbers");
-            System.out.println("Enter first number=");
+            System.out.println("Enter first number: ");
             int num1=sc.nextInt();
-            System.out.println("Enter second number=");
+            System.out.println("Enter second number: ");
             int num2=sc.nextInt();
-            System.out.println("Enter third number=");
+            System.out.println("Enter third number: ");
             int num3=sc.nextInt();
 
-            //to find greatest
+            //to find greatest number
             if (num1 >= num2 && num1 >= num3) 
             {
                 System.out.println("The greatest number is: " + num1);
@@ -39,20 +39,18 @@ public class Operations {
             {
                 System.out.println("The greatest number is: " + num3);
             }
-    
             break;
-
         case 2:
             System.out.println("To check given no. is even or odd");
-            System.out.println("Enter number=");
+            System.out.println("Enter number : ");
             int num=sc.nextInt();
             if (num%2==0)
             {
-                System.out.println("Given no.+num+ " +num+ "  is even");      
+                System.out.println("Given number " +num+ "  is even");      
             }
             else
             {
-                System.out.println("Given no."  +num+ " is odd");
+                System.out.println("Given number"  +num+ " is odd");
             }
             break;
 
@@ -61,7 +59,7 @@ public class Operations {
             System.out.println("enter character=");
             char ch=sc.next().charAt(0);
 
-            if (ch == 'a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') 
+            if (ch == 'a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch == 'A' || ch=='E' || ch=='I' || ch=='O' || ch=='U') 
             {
                 System.out.println("Given character" +ch+ " is vowel");
             }
@@ -71,17 +69,17 @@ public class Operations {
             }
             break;
         case 4:
-            System.out.println("To check divisibility by 5");
-            System.out.println("Enter number=");
+            System.out.println("To check divisible by 5");
+            System.out.println("Enter number: ");
             int number=sc.nextInt();
 
             if (number%5==0)
             {
-                System.out.println(+number+ " is divisible by 5");
+                System.out.println(number+ " is divisible by 5");
             }
             else
             {
-                System.out.println(+number+ " is not dvisible by 5");
+                System.out.println(number+ " is not dvisible by 5");
             }
             break;
 
